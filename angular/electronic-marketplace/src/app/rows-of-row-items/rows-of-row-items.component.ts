decaa1ccb7f0fc5939bc7729f87042a6f45589fd
Luis <luis.ramirez@revature.net>
2021-10-25 17:23:06 -0400

    Co-authored-by: madlamane <madlamane@users.noreply.github.com>

---
+++ angular/electronic-marketplace/src/app/rows-of-row-items/rows-of-row-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rows-of-row-items',
+  templateUrl: './rows-of-row-items.component.html',
+  styleUrls: ['./rows-of-row-items.component.css']
+})
+export class RowsOfRowItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
